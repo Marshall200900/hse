f5055a2a6b3e7bfde2f88edd1a1a84510f8b6fd0
Arseniy Sidorov <arseniy.sidorov2013@gmail.com>
2021-12-07 21:10:14 +0500

    lanin-lab3

---
+++ Технологии анализа данных в сети интернет/GetUsersInfo.py
@@ -0,0 +1,122 @@
+# Импортируем нужные модули
+from urllib.request import urlretrieve
+from collections import Counter
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from time import sleep
+import matplotlib.pyplot as plt
+
+import vk, math
+
+# Авторизация
+
+# login = ''
+# password = ''
+# vk_id = ''
+
+# session = vk.AuthSession(app_id=vk_id, user_login=login, user_password=password) 
+# vkapi = vk.API(session, v='5.81')
+
+session = vk.Session(access_token='8ad739e5417b57f55e87601627432331fba8fbfee4e9794133290021743513c5f275843568f5d8cce5c8c')
+vkapi = vk.API(session, v='5.81')
+
+def getUsers(group_id, count=1000):
+    users = []
+
+    for i in range(math.ceil(count / 1000)):
+        
+        users.extend(vkapi.groups.getMembers(group_id=group_id, count=1000, sort='id_desc', offset=i*1000)['items'])
+        print('got {} users'.format(1000*(i+1)))
+        if i % 10 == 0 and i != 0:
+            print('sleeping 2s...')
+            sleep(2)
+    return users
+
+def findIntersectionPercent(group_id1,group_id2):
+    users1 = getUsers(group_id1, 1_000)
+    print('sleeping 2s...')
+    sleep(2)
+    users2 = getUsers(group_id2, 1_000)
+    intersection = set(users1).intersection(set(users2))
+    return len(intersection) / len(users1) * 100
+
+# Meduza Info
+meduza_users = vkapi.groups.getMembers(group_id='meduzaproject', fields='sex,bdate,city', count=1000, sort='id_desc')['items']
+meduza_sex = [i['sex'] for i in meduza_users]
+meduza_cities = list(filter(lambda x: x != None, [ i['city']['title'] if 'city' in i else None for i in meduza_users]))
+meduza_bdate = list(filter(lambda x: x != None, [ i['bdate'] if 'bdate' in i and len(i['bdate'].split('.')) == 3 else None for i in meduza_users]))
+meduza_ages_years = [relativedelta(datetime.now(), datetime.strptime(i, '%d.%m.%Y')).years for i in meduza_bdate]    
+meduza_city_rate = Counter(meduza_cities)
+
+meduza_female_count = meduza_sex.count(1)
+meduza_male_count = len(meduza_sex) - meduza_female_count
+
+print('Meduza:')
+print('Female percentage: {:.1f}'.format(meduza_female_count / 1000 * 100))
+print('Male percentage: {:.1f}'.format(meduza_male_count / 1000 * 100))
+print('Top countries: {}'.format(meduza_city_rate.most_common(3)))
+print('Average age: {:.1f}'.format(sum(meduza_ages_years) / len(meduza_ages_years)))
+
+
+# Perm Active Info
+perm_users = vkapi.groups.getMembers(group_id='permactive', fields='sex,bdate,city', count=1000, sort='id_desc')['items']
+perm_sex = [i['sex'] for i in perm_users]
+perm_cities = list(filter(lambda x: x != None, [ i['city']['title'] if 'city' in i else None for i in perm_users]))
+perm_bdate = list(filter(lambda x: x != None, [ i['bdate'] if 'bdate' in i and len(i['bdate'].split('.')) == 3 else None for i in perm_users]))
+perm_ages_years = [relativedelta(datetime.now(), datetime.strptime(i, '%d.%m.%Y')).years for i in perm_bdate]    
+perm_city_rate = Counter(perm_cities)
+
+perm_female_count = perm_sex.count(1)
+perm_male_count = len(perm_sex) - perm_female_count
+
+print('Perm Active:')
+print('Female percentage: {:.1f}'.format(perm_female_count / 1000 * 100))
+print('Male percentage: {:.1f}'.format(perm_male_count / 1000 * 100))
+print('Top countries: {}'.format(perm_city_rate.most_common(3)))
+print('Average age: {:.1f}'.format(sum(perm_ages_years) / len(perm_ages_years)))
+
+
+print('{:.1f} percent of Meduza intersects Perm Active'.format(findIntersectionPercent('meduzaproject', 'permactive')))
+
+
+fig, ax = plt.subplots(3)
+
+# plot 1
+ax[0].set_title('Average age comparison')
+ax[0].bar(['Meduza', 'Perm Active'], [sum(meduza_ages_years) / len(meduza_ages_years), sum(perm_ages_years) / len(perm_ages_years)])
+# plot 2
+ax[1].set_title('Cities of Meduza')
+top5 = meduza_city_rate.most_common(5)
+labels = [i[0] for i in top5]
+values = [i[1] for i in top5]
+
+others = meduza_city_rate
+for i in top5:
+    others.pop(i[0])
+
+sum_others = sum(list(dict(others).values()))
+
+labels.append('Другие')
+values.append(sum_others)
+
+ax[1].pie(values, labels=labels)
+
+
+
+ax[2].set_title('Cities of Perm Active')
+top5 = perm_city_rate.most_common(3)
+labels = [i[0] for i in top5]
+values = [i[1] for i in top5]
+
+others = perm_city_rate
+for i in top5:
+    others.pop(i[0])
+
+sum_others = sum(list(dict(others).values()))
+
+labels.append('Другие')
+values.append(sum_others)
+
+ax[2].pie(values, labels=labels)
+
+plt.show()
---
+++ Технологии анализа данных в сети интернет/LoadPhotos.py
@@ -0,0 +1,65 @@
+# Импортируем нужные модули
+from urllib.request import urlretrieve
+import vk, os, time, math
+
+# Авторизация
+
+# login = ''
+# password = ''
+# vk_id = ''
+
+# session = vk.AuthSession(app_id=vk_id, user_login=login, user_password=password) 
+# vkapi = vk.API(session, v='5.81')
+
+session = vk.Session(access_token='')
+vkapi = vk.API(session, v='5.81')
+
+url = "https://vk.com/album-132_47581240"
+# Разбираем ссылку
+owner_id = url.split('/')[-1].split('_')[0].replace('album', '')
+
+albums = vkapi.photos.getAlbums(owner_id=owner_id)
+
+
+
+if not os.path.exists('saved'):
+    os.mkdir('saved')
+
+time_now = time.time() # время старта
+
+photos = vkapi.photos.get(owner_id=owner_id, album_id=albums['items'][1]['id'], count=1000, offset=0)
+counter_total = 0
+broken_total = 0
+
+
+for album in albums['items']:
+    photo_folder = 'saved/album{0}_{1}'.format(owner_id, album['id'])
+    if not os.path.exists(photo_folder):
+        os.mkdir(photo_folder)
+    photos_count = album['size']
+
+    
+
+    counter = 0 # текущий счетчик
+    breaked = 0 # не загружено из-за ошибки
+    prog = 0 # процент загруженных
+    
+    for j in range(math.ceil(photos_count / 1000)): # Подсчитаем&nbsp;сколько раз нужно получать список фото, так как число получится не целое - округляем в большую сторону
+        photos = vkapi.photos.get(owner_id=owner_id, album_id=album['id'], count=1000, offset=j*1000) #&nbsp;Получаем список фото
+        for photo in photos["items"]:
+            counter += 1
+            url = photo["sizes"][-1]["url"] # Получаем адрес изображения
+            print('Загружаю фото № {} из {} альбома {}. Прогресс: {} %'.format(counter, photos_count, album['id'], prog))
+            prog = round(100/photos_count*counter,2)
+            try:
+                urlretrieve(url, photo_folder + "/" + os.path.split(url)[1].split('?')[0]) # Загружаем и сохраняем файл
+            except Exception:
+                print(url)
+                print('Произошла ошибка, файл пропущен.')
+                breaked += 1
+    
+    counter_total += counter
+    broken_total += breaked
+
+time_for_dw = time.time() - time_now
+print("\nВ очереди было {} файлов. Из них удачно загружено {} файлов, {} не удалось загрузить. Затрачено времени: {} сек.". format(counter_total, counter_total-broken_total, broken_total, round(time_for_dw,1)))
