7bc27b850338d91ed6d49f4e8e8874652b0cb035
Arseniy Sidorov <arseniy.sidorov2013@gmail.com>
2021-12-07 21:10:53 +0500

    upp-lab1

--- Распределенные вычисления/1_Pipes/PipesClient/PipesClient/Client.Designer.cs
+++ Распределенные вычисления/1_Pipes/PipesClient/PipesClient/Client.Designer.cs
@@ -30,19 +30,16 @@
         {
             this.btnSend = new System.Windows.Forms.Button();
             this.lblPipe = new System.Windows.Forms.Label();
-            this.serverBox = new System.Windows.Forms.TextBox();
-            this.messageBox = new System.Windows.Forms.TextBox();
-            this.recieveBox = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.loginLabel = new System.Windows.Forms.Label();
+            this.tbPipe = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.tbMessage = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(387, 366);
-            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSend.Location = new System.Drawing.Point(284, 53);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(100, 28);
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
             this.btnSend.TabIndex = 2;
             this.btnSend.Text = "Отправить";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -51,75 +48,49 @@
             // lblPipe
             // 
             this.lblPipe.AutoSize = true;
-            this.lblPipe.Location = new System.Drawing.Point(16, 11);
-            this.lblPipe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPipe.Location = new System.Drawing.Point(12, 9);
             this.lblPipe.Name = "lblPipe";
-            this.lblPipe.Size = new System.Drawing.Size(92, 34);
+            this.lblPipe.Size = new System.Drawing.Size(72, 26);
             this.lblPipe.TabIndex = 1;
             this.lblPipe.Text = "Введите имя\r\nканала";
             // 
-            // serverBox
+            // tbPipe
             // 
-            this.serverBox.Location = new System.Drawing.Point(120, 18);
-            this.serverBox.Margin = new System.Windows.Forms.Padding(4);
-            this.serverBox.Name = "serverBox";
-            this.serverBox.Size = new System.Drawing.Size(132, 22);
-            this.serverBox.TabIndex = 0;
-            this.serverBox.Text = "\\\\.\\pipe\\ServerPipe";
+            this.tbPipe.Location = new System.Drawing.Point(90, 15);
+            this.tbPipe.Name = "tbPipe";
+            this.tbPipe.Size = new System.Drawing.Size(188, 20);
+            this.tbPipe.TabIndex = 0;
+            this.tbPipe.Text = "\\\\.\\pipe\\ServerPipe";
             // 
-            // messageBox
+            // lblMessage
             // 
-            this.messageBox.Location = new System.Drawing.Point(19, 369);
-            this.messageBox.Margin = new System.Windows.Forms.Padding(4);
-            this.messageBox.Name = "messageBox";
-            this.messageBox.Size = new System.Drawing.Size(350, 22);
-            this.messageBox.TabIndex = 1;
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(12, 58);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(65, 13);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "Сообщение";
             // 
-            // recieveBox
+            // tbMessage
             // 
-            this.recieveBox.Location = new System.Drawing.Point(19, 48);
-            this.recieveBox.Name = "recieveBox";
-            this.recieveBox.Size = new System.Drawing.Size(468, 311);
-            this.recieveBox.TabIndex = 3;
-            this.recieveBox.Text = "";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(260, 21);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 17);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Логин";
-            // 
-            // loginLabel
-            // 
-            this.loginLabel.AutoSize = true;
-            this.loginLabel.Location = new System.Drawing.Point(315, 21);
-            this.loginLabel.Name = "loginLabel";
-            this.loginLabel.Size = new System.Drawing.Size(73, 17);
-            this.loginLabel.TabIndex = 5;
-            this.loginLabel.Text = "loginLabel";
+            this.tbMessage.Location = new System.Drawing.Point(90, 55);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(188, 20);
+            this.tbMessage.TabIndex = 1;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(511, 416);
-            this.Controls.Add(this.loginLabel);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.recieveBox);
-            this.Controls.Add(this.messageBox);
-            this.Controls.Add(this.serverBox);
+            this.ClientSize = new System.Drawing.Size(369, 90);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.tbPipe);
             this.Controls.Add(this.lblPipe);
             this.Controls.Add(this.btnSend);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Клиент";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
-            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,10 +100,8 @@
 
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.Label lblPipe;
-        private System.Windows.Forms.TextBox serverBox;
-        private System.Windows.Forms.TextBox messageBox;
-        private System.Windows.Forms.RichTextBox recieveBox;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.TextBox tbPipe;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox tbMessage;
     }
 }
--- Распределенные вычисления/1_Pipes/PipesClient/PipesClient/Client.cs
+++ Распределенные вычисления/1_Pipes/PipesClient/PipesClient/Client.cs
@@ -9,99 +9,29 @@ using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
-using System.Threading;
-using PipesClient;
 
 namespace Pipes
 {
     public partial class frmMain : Form
     {
         private Int32 PipeHandle;   // дескриптор канала
-        private Thread t;                                                               // поток для обслуживания канала
-        private bool _continue = true;                                                  // флаг, указывающий продолжается ли работа с каналом
-        public static string CHANNEL_NAME = "";
+
         // конструктор формы
         public frmMain()
         {
-            Form1 form = new Form1();
-            var result = form.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                CHANNEL_NAME = form.login;
-            }
             InitializeComponent();
-            loginLabel.Text = CHANNEL_NAME;
-            
             this.Text += "     " + Dns.GetHostName();   // выводим имя текущей машины в заголовок формы
         }
 
         private void btnSend_Click(object sender, EventArgs e)
         {
-            if (messageBox.Text == "")
-            {
-                MessageBox.Show("Введите сообщение");
-                return;
-            }
-            SendMessage(serverBox.Text, CHANNEL_NAME, messageBox.Text);
-            messageBox.Text = "";
-        }
-
-        private void MessageListener()
-        {
-            string msg = "";            // прочитанное сообщение
-            uint realBytesReaded = 0;   // количество реально прочитанных из канала байтов
-
-            // входим в бесконечный цикл работы с каналом
-            while (_continue)
-            {
-                if (DIS.Import.ConnectNamedPipe(PipeHandle, 0))
-                {
-                    byte[] buff = new byte[1024];                                           // буфер прочитанных из канала байтов
-                    DIS.Import.FlushFileBuffers(PipeHandle);                                // "принудительная" запись данных, расположенные в буфере операционной системы, в файл именованного канала
-                    DIS.Import.ReadFile(PipeHandle, buff, 1024, ref realBytesReaded, 0);    // считываем последовательность байтов из канала в буфер buff
-                    msg = Encoding.Unicode.GetString(buff);                                 // выполняем преобразование байтов в последовательность символов
-                    recieveBox.Invoke((MethodInvoker)delegate
-                    {
-                        if (msg != "")
-                            recieveBox.Text += "\n >> " + msg;                             // выводим полученное сообщение на форму
-                    });
-
-                    DIS.Import.DisconnectNamedPipe(PipeHandle);                             // отключаемся от канала клиента 
-                    Thread.Sleep(500);                                                      // приостанавливаем работу потока перед тем, как приcтупить к обслуживанию очередного клиента
-                }
-            }
-        }
-
-        private void SendMessage(string channelName, string login, string message)
-        {
             uint BytesWritten = 0;  // количество реально записанных в канал байт
-            byte[] buff = Encoding.Unicode.GetBytes(login + " >> " + message);    // выполняем преобразование сообщения (вместе с идентификатором машины) в последовательность байт
+            byte[] buff = Encoding.Unicode.GetBytes(Dns.GetHostName().ToString() + " >> " + tbMessage.Text);    // выполняем преобразование сообщения (вместе с идентификатором машины) в последовательность байт
 
             // открываем именованный канал, имя которого указано в поле tbPipe
-            int pipeSender = DIS.Import.CreateFile(channelName, DIS.Types.EFileAccess.GenericWrite, DIS.Types.EFileShare.Read, 0, DIS.Types.ECreationDisposition.OpenExisting, 0, 0);
-            DIS.Import.WriteFile(pipeSender, buff, Convert.ToUInt32(buff.Length), ref BytesWritten, 0);         // выполняем запись последовательности байт в канал
-            DIS.Import.CloseHandle(pipeSender);              // закрываем дескриптор канала
-        }
-
-        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            _continue = false;      // сообщаем, что работа с каналом завершена
-
-            if (t != null)
-                t.Abort();          // завершаем поток
-
-            if (PipeHandle != -1)
-                DIS.Import.CloseHandle(PipeHandle);     // закрываем дескриптор канала
-        }
-
-        private void frmMain_Load(object sender, EventArgs e)
-        {
-            SendMessage(serverBox.Text, CHANNEL_NAME, CHANNEL_NAME + " зашел пообщаться в чат");
-            // создание именованного канала
-            PipeHandle = DIS.Import.CreateNamedPipe("\\\\.\\pipe\\" + CHANNEL_NAME, DIS.Types.PIPE_ACCESS_DUPLEX, DIS.Types.PIPE_TYPE_BYTE | DIS.Types.PIPE_WAIT, DIS.Types.PIPE_UNLIMITED_INSTANCES, 0, 1024, DIS.Types.NMPWAIT_WAIT_FOREVER, (uint)0);
-            // создание потока, отвечающего за работу с каналом
-            t = new Thread(MessageListener);
-            t.Start();
+            PipeHandle = DIS.Import.CreateFile(tbPipe.Text, DIS.Types.EFileAccess.GenericWrite, DIS.Types.EFileShare.Read, 0, DIS.Types.ECreationDisposition.OpenExisting, 0, 0);
+            DIS.Import.WriteFile(PipeHandle, buff, Convert.ToUInt32(buff.Length), ref BytesWritten, 0);         // выполняем запись последовательности байт в канал
+            DIS.Import.CloseHandle(PipeHandle);                                                                 // закрываем дескриптор канала
         }
     }
 }
